ad7c0bfbac79b8894a146fc06b415561e251dc8e
gunjank <sngunjan@yahoo.com>
2016-02-21 23:21:31 -0500

    updated with working express and angular code

--- app.js
+++ app.js
@@ -1,26 +1,17 @@
 var express = require('express');
-var bootstrapService = require("express-bootstrap-service");
+var bootstrap = require("express-bootstrap-service");
 var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-var routes = require('./routes/index');
-var users = require('./routes/users');
+
 
 var app = express();
-app.use(bootstrapService.serve);
-bootstrap.init({
-	minified: false
-});
-var msg = 'hello world';
-console.log(msg);
-
-
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
+app.set('public', path.join(__dirname, 'public'));
 app.set('view engine', 'jade');
 
 // uncomment after placing your favicon in /public
@@ -31,8 +22,14 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', routes);
-app.use('/users', users);
+
+app.get('/', function(req, res, next){
+    if(req.param )
+    {
+        res.sendFile(req.app.get('public')+'/views/index.html');
+    }
+     
+});
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -48,7 +45,7 @@ app.use(function(req, res, next) {
 if (app.get('env') === 'development') {
   app.use(function(err, req, res, next) {
     res.status(err.status || 500);
-    res.render('error', {
+    res.render(req.app.get('public')+'/views/error', {
       message: err.message,
       error: err
     });
@@ -59,7 +56,7 @@ if (app.get('env') === 'development') {
 // no stacktraces leaked to user
 app.use(function(err, req, res, next) {
   res.status(err.status || 500);
-  res.render('error', {
+  res.render(req.app.get('public')+'/views/error', {
     message: err.message,
     error: {}
   });
